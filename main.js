a50af1bcc62358a33ea2b559c82b002c83d362db
Karina <kbrilhante@gmail.com>
2022-07-09 12:07:31 -0300

    Estrutura de  Dados 2

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
 var arrayAlunos = [];
 
-var nome = [];
-function enviar() {}
+function enviar() {
+    // var nome1 = document.getElementById("aluno1").value;
+    // var nome2 = document.getElementById("aluno2").value;
+    // var nome3 = document.getElementById("aluno3").value;
+    // var nome4 = document.getElementById("aluno4").value;
+
+    // arrayAlunos.push(nome1);
+    // arrayAlunos.push(nome2);
+    // arrayAlunos.push(nome3);
+    // arrayAlunos.push(nome4);
+    var arrayHTML = [];
+
+    for (var cont=1; cont<=4; cont++) {
+        var nome;
+        nome = document.getElementById("aluno" + cont).value;
+        console.log(nome);
+        arrayAlunos.push(nome);
+    }
+
+    console.log(arrayAlunos);
+    
+    var tamanhoArray = arrayAlunos.length;
+
+    for (var i=0; i<tamanhoArray; i++) {
+        arrayHTML.push("<h4>" + arrayAlunos[i] + "</h4>");
+    }
+
+    console.log(arrayHTML);
+
+    document.getElementById("btnEnviar").style.display = "none";
+    document.getElementById("btnOrganizar").style.display = "inline-block";
+}
+
+function organizar() {
+    arrayAlunos.sort();
+    console.log(arrayAlunos);
+    document.getElementById("mostraNomes").innerHTML = arrayAlunos;
+}
